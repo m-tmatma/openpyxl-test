da6e08bf5a200260c0c02956f256178ae740933e
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-12-11 09:19:57 +0900

    change output xlsx filename

--- new.py
+++ new.py
@@ -1,5 +1,9 @@
 import openpyxl
+import os
+import sys
+
+output_xlsx = "output_ " + os.path.basename(sys.argv[0]) + '.xlsx'
 
 wb = openpyxl.Workbook()
 ws = wb.active
-wb.save('Sample1.xlsx')
+wb.save(output_xlsx)
--- rename-sheet.py
+++ rename-sheet.py
@@ -1,6 +1,10 @@
 import openpyxl
+import os
+import sys
+
+output_xlsx = "output_ " + os.path.basename(sys.argv[0]) + '.xlsx'
 
 wb = openpyxl.Workbook()
 ws = wb.worksheets[0]
 ws.title = 'testsheet'
-wb.save('Sample1.xlsx')
+wb.save(output_xlsx)

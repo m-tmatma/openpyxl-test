1203644b8d509d914e3e018a07fc029385462fad
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-12-15 18:46:10 +0900

    add docstring

--- rename_sheet.py
+++ rename_sheet.py
@@ -1,3 +1,9 @@
+'''
+    https://qiita.com/github-nakasho/items/3f861395227e5645cce7
+    https://qiita.com/github-nakasho/items/358e5602aeda81c58c81
+    https://stackoverflow.com/questions/51566349/openpyxl-how-to-add-filters-to-all-columns
+'''
+
 import os
 import sys
 import openpyxl

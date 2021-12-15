5aaaa666d618fb02392020dbd111af82f8a9a883
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-12-15 19:04:36 +0900

    chnage tab to space as indentation.

--- plot_to_xlsx.py
+++ plot_to_xlsx.py
@@ -16,71 +16,71 @@ from openpyxl.utils.units import EMU_to_pixels
 from openpyxl.drawing.spreadsheet_drawing import OneCellAnchor, AnchorMarker
 
 def plot(multiple):
-	fig, ax = plt.subplots(figsize=(4, 3))
-	x=np.array([1,2,3,4,5])
-	y=np.array([2,4,5,1,2]) * multiple
-	ax.plot(x, y)
-	img_data = io.BytesIO()
-	fig.savefig(img_data, format='png')
+    fig, ax = plt.subplots(figsize=(4, 3))
+    x=np.array([1,2,3,4,5])
+    y=np.array([2,4,5,1,2]) * multiple
+    ax.plot(x, y)
+    img_data = io.BytesIO()
+    fig.savefig(img_data, format='png')
 
-	img = openpyxl.drawing.image.Image(img_data)
-	
-	# https://matplotlib.org/stable/api/figure_api.html#matplotlib.figure.Figure.figimage
-	# https://www.unitconverters.net/typography/inch-to-pixel-x.htm
-	img.width  = fig.get_figwidth()  * fig.dpi
-	img.height = fig.get_figheight() * fig.dpi
-	print(img.width, img.height)
-	return img
+    img = openpyxl.drawing.image.Image(img_data)
+    
+    # https://matplotlib.org/stable/api/figure_api.html#matplotlib.figure.Figure.figimage
+    # https://www.unitconverters.net/typography/inch-to-pixel-x.htm
+    img.width  = fig.get_figwidth()  * fig.dpi
+    img.height = fig.get_figheight() * fig.dpi
+    print(img.width, img.height)
+    return img
 
 def insert_plt(worksheet, num=10, start_row=1, start_column=1):
-	green = '92d050' # R = 146, G = 208, B = 80
-	yello = 'ffff00' # R = 255, G = 255, B = 0
+    green = '92d050' # R = 146, G = 208, B = 80
+    yello = 'ffff00' # R = 255, G = 255, B = 0
 
-	fill      = openpyxl.styles.PatternFill(patternType='solid', fgColor=yello)
-	fillTitle = openpyxl.styles.PatternFill(patternType='solid', fgColor=green)
+    fill      = openpyxl.styles.PatternFill(patternType='solid', fgColor=yello)
+    fillTitle = openpyxl.styles.PatternFill(patternType='solid', fgColor=green)
 
-	side   = openpyxl.styles.borders.Side(style='thin', color='000000')
-	border = openpyxl.styles.borders.Border(top=side, bottom=side, left=side, right=side)
+    side   = openpyxl.styles.borders.Side(style='thin', color='000000')
+    border = openpyxl.styles.borders.Border(top=side, bottom=side, left=side, right=side)
 
-	for i in range(1, num+1):
-		multiple = i + 1
+    for i in range(1, num+1):
+        multiple = i + 1
 
-		img = plot(multiple)
+        img = plot(multiple)
 
-		p2e = pixels_to_EMU
-		e2p = EMU_to_pixels
+        p2e = pixels_to_EMU
+        e2p = EMU_to_pixels
 
-		h, w = img.height, img.width
-		size = XDRPositiveSize2D(p2e(w), p2e(h))
-		row = i+1
+        h, w = img.height, img.width
+        size = XDRPositiveSize2D(p2e(w), p2e(h))
+        row = i+1
 
-		worksheet.column_dimensions["B"].width     = 80
-		worksheet.row_dimensions[row].height       = h
+        worksheet.column_dimensions["B"].width     = 80
+        worksheet.row_dimensions[row].height       = h
 
-		row    = start_row + i - 1
-		column = start_column
+        row    = start_row + i - 1
+        column = start_column
 
-		c2e = cm_to_EMU
+        c2e = cm_to_EMU
 
-		# Calculated number of cells width or height from cm into EMUs
-		# Want to place image in row 5 (6 in excel), column 2 (C in excel)
-		# Also offset by half a column.
+        # Calculated number of cells width or height from cm into EMUs
+        # Want to place image in row 5 (6 in excel), column 2 (C in excel)
+        # Also offset by half a column.
 
-		cellh= lambda x: c2e((x * 49.77)/99)
-		cellw= lambda x: c2e((x * (18.65-1.71))/10)
+        cellh= lambda x: c2e((x * 49.77)/99)
+        cellw= lambda x: c2e((x * (18.65-1.71))/10)
 
-		coloffset= cellw(0.2)
-		rowoffset= cellh(0.2)
+        coloffset= cellw(0.2)
+        rowoffset= cellh(0.2)
 
-		marker= AnchorMarker(col=column, row=row, colOff=coloffset, rowOff=rowoffset)
-		img.anchor= OneCellAnchor(_from=marker, ext=size)
-		worksheet.add_image(img)
-		
-		cell = worksheet.cell(row=i+1,column=1)
-		cell.border = border
+        marker= AnchorMarker(col=column, row=row, colOff=coloffset, rowOff=rowoffset)
+        img.anchor= OneCellAnchor(_from=marker, ext=size)
+        worksheet.add_image(img)
+        
+        cell = worksheet.cell(row=i+1,column=1)
+        cell.border = border
 
-		cell = worksheet.cell(row=i+1,column=2)
-		cell.border = border
+        cell = worksheet.cell(row=i+1,column=2)
+        cell.border = border
 
 
 output_xlsx = "output_ " + os.path.basename(sys.argv[0]) + '.xlsx'

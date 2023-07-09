247526e1250e546373101a63459172b16d8417a4
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-07-09 10:24:51 +0900

    CellIsRule

--- openpyxl_conditional_formatting.py
+++ openpyxl_conditional_formatting.py
@@ -6,8 +6,9 @@ from openpyxl import Workbook
 from openpyxl.styles import PatternFill, GradientFill
 from openpyxl.styles import Alignment
 #from openpyxl.styles.differential import DifferentialStyle
-#from openpyxl.formatting.rule import ColorScaleRule, CellIsRule, Rule
+#from openpyxl.formatting.rule import ColorScaleRule, Rule
 from openpyxl.formatting.rule import FormulaRule
+from openpyxl.formatting.rule import CellIsRule
 
 wb = Workbook()
 ws = wb.active
@@ -18,6 +19,7 @@ gadientFill = GradientFill(type='linear', stop=("00FF00", "FF0000"))
 ws.conditional_formatting.add('D1:D10', FormulaRule(formula=['NOT(ISBLANK(D1))'], stopIfTrue=True, fill=redFill))
 ws.conditional_formatting.add('E1:E10', FormulaRule(formula=['ISBLANK(E1)'], stopIfTrue=True, fill=redFill))
 ws.conditional_formatting.add('F1:F10', FormulaRule(formula=['F1=1'], stopIfTrue=True, fill=gadientFill))
+ws.conditional_formatting.add('G1:G10', CellIsRule(operator='lessThan', formula=[5], stopIfTrue=True, fill=redFill))
 
 ws['D1'] = "A"
 ws['D2'] = "B"
@@ -29,5 +31,8 @@ ws['E2'] = "B"
 
 ws['F1'] = 0
 ws['F2'] = 1
+
+for i in range(1, 10+1):
+	ws[f'G{i}'] = i
 
 wb.save("test.xlsx")
